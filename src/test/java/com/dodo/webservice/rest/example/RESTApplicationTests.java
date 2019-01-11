283551f41a4f94d9adcc67209ed2dc6db31c10cb
Andrea Dodero <dodero.andrea@gmail.com>
2019-01-11 14:49:00 +1100

    rename test class

--- src/test/java/com/dodo/webservice/rest/example/RESTApplicationTests.java
+++ src/test/java/com/dodo/webservice/rest/example/RESTApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PaloitApplicationTests {
+public class RESTApplicationTests {
 
 	@Test
 	public void contextLoads() {
